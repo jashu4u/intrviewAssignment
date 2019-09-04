73a3cb1c18ad28b899a66b0e1c272e94101c9bd5
jashu4u <53554852+jashu4u@users.noreply.github.com>
2019-09-04 14:34:53 +0530

    Added ibx_request
    Added ibx_request

--- commonUtils.pm
+++ commonUtils.pm
@@ -117,4 +117,97 @@ sub logEvent {
     return 1;
 }
 
+
+########################################################
+# To execute ibx wapis
+########################################################
+sub ibxRequest {
+
+    my ($_objType
+       ,$_params
+       ,$_body)  = (@_);
+
+    $_body       = ($_body ? to_json($_body) : '{}');
+
+    our $ibx_user = $configProperties->{ibxUser};
+    our $ibx_passwd = $configProperties->{ibxUser};
+    our $ibx_url = $configProperties->{ibxUrl};
+
+    my $results;
+    my $status;
+
+
+    if( ! $client ) {
+
+        $client      = REST::Client->new({timeout => 3600});
+        $client->setHost($ibx_url);
+        $client->getUseragent()->ssl_opts(verify_hostname => 0);
+        $client->getUseragent()->ssl_opts(SSL_verify_mode => 'SSL_VERIFY_NONE');
+
+    }
+
+    my $url2    = '/' . $_objType . $client->buildQuery($_params);
+    my $headers = {Authorization => 'Basic '.  encode_base64($ibx_user . ':' . $ibx_passwd),};
+    if( lc($_params->{_method}) eq 'delete' ) {
+#
+# DELETE
+#
+        $client->DELETE($url2, $headers);
+
+        my $content      = $client->responseContent();
+        $status          = ($client->responseCode() == 200  ? 1 : 0);
+
+        if( $status ) {
+
+            $results         = {_ref => $content};
+
+        } else {
+
+            $results         = from_json($content);
+
+        }
+
+    } elsif( lc($_params->{_method}) eq 'get' ) {
+#
+# GET
+#
+        $client->GET($url2 , $headers);
+        my $content_json = $client->responseContent();
+
+        eval {
+             from_json($content_json);
+             1;
+        } or do {
+             return 0, [{'Error' => $content_json}];
+        };
+
+        my $content      = from_json($content_json);
+        $status          = ($content =~ /HASH.*/ && defined($content->{'Error'}) ? 0 : 1);
+        $results         = ($content =~ /ARRAY.*/ ? $content : [$content]);
+
+
+    } elsif( lc($_params->{_method}) eq 'post' ) {
+#
+# POST
+#
+        $client->POST($url2, $_body, $headers);
+        my $content_json = $client->responseContent();
+        $status          = ($client->responseCode() >= 200 && $client->responseCode() <= 299  ? 1 : 0);
+        $results         = from_json($content_json);
+
+    } elsif( lc($_params->{_method}) eq 'put' ) {
+#
+# PUT
+#
+        $client->POST($url2, $_body, $headers);
+
+        my $content_json = $client->responseContent();
+        $status          = ($client->responseCode() >= 200 && $client->responseCode() <= 299  ? 1 : 0);
+        $results         = from_json($content_json);
+
+    }
+    return $status, $results;
+
+}
+
 1;
